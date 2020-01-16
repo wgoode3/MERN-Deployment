2d899e5201f48c94f854085f18b16e177643bcba
William Goode <wgoode3@gmail.com>
2020-01-16 09:52:33 -0500

    added build

---
+++ client/build/precache-manifest.8f6f62b2f733ec99c1ad8bfb0a0837dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9166f30d52bef2c5eb267fd46db90e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e12843c12baba96756b",
+    "url": "/static/css/2.d2e2750e.chunk.css"
+  },
+  {
+    "revision": "429f8ee714f894fdf4fb",
+    "url": "/static/css/main.c4cc2b14.chunk.css"
+  },
+  {
+    "revision": "6e12843c12baba96756b",
+    "url": "/static/js/2.4f23074a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.4f23074a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "429f8ee714f894fdf4fb",
+    "url": "/static/js/main.c99842cd.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
